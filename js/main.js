2b2e4b844283db44a1d740d40681b56e9bb37ecf
Juma Allan <jumaallanie1708@gmail.com>
2022-02-17 11:50:27 +0300

    starting to code today so cool

---
+++ js/main.js
@@ -0,0 +1,6 @@
+git remote set-url origin https://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@github.com/github.com/juma-moringa
+
+
+
+
+git pull https://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@.gitgithub.com/juma-moringa
